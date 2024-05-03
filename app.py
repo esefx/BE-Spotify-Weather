a76e16f43737418d50c9b5a78d1c5dd1520d5bca
Your Name <susi.franco@cerebral.com>
2024-05-02 22:02:55 -0500

    register routes

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from dotenv import load_dotenv
 import os
+from flask import Flask
+from routes.weather import weather_routes
 
 #load env variables from .env
 load_dotenv()
@@ -7,3 +9,12 @@ load_dotenv()
 #get api keys
 LOCATION_API_KEY = os.getenv('LOCATIONIQ_API_KEY')
 OPENWEATHER_API_KEY = os.getenv('OPENWEATHER_API_KEY')
+
+#initialize flask app
+app = Flask(__name__)
+
+#register all routes
+app.register_blueprint(weather_routes)
+
+if __name__ == '__main__':
+    app.run(debug=True)
