ddc17905202214beae4edba33e0c6dfa44644d15
Your Name <susi.franco@cerebral.com>
2024-05-20 22:10:14 -0500

    get rid of get all user's playlists and refractor

--- routes/spotify.py
+++ routes/spotify.py
@@ -1,18 +1,16 @@
-from flask import Blueprint, redirect, request, jsonify, session
-from flask_session import Session
+from flask import Blueprint, redirect, request, jsonify, session, url_for
 import os
 import logging
 import urllib.parse
 import datetime
 import requests
 
-
 logging.basicConfig(level=logging.DEBUG)
 
 # Spotify API endpoints
 AUTH_URL = 'https://accounts.spotify.com/authorize'
 TOKEN_URL = 'https://accounts.spotify.com/api/token'
-API_BASE_URL = 'https://api.spotify.com/v1/'  # Correct the version here
+API_BASE_URL = 'https://api.spotify.com/v1/'
 
 # Get Spotify API keys
 SPOTIFY_CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
@@ -20,10 +18,9 @@ SPOTIFY_CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
 SPOTIFY_REDIRECT_URI = os.getenv('SPOTIFY_REDIRECT_URI')
 SPOTIFY_SCOPES = os.getenv('SPOTIFY_SCOPES')
 
-
 spotify_routes = Blueprint('spotify_routes', __name__)
 
-#handle login
+# Handle login
 @spotify_routes.route('/login', methods=['GET'])
 def login():
     params = {
@@ -33,11 +30,10 @@ def login():
         'scope': SPOTIFY_SCOPES,
         'show_dialog': True
     }
-
     auth_url = f"{AUTH_URL}?{urllib.parse.urlencode(params)}"
     return redirect(auth_url)
 
-#handle if login is successful or not
+# Handle if login is successful or not
 @spotify_routes.route('/callback', methods=['GET'])
 def callback():
     if 'error' in request.args:
@@ -51,7 +47,6 @@ def callback():
             'client_id': SPOTIFY_CLIENT_ID,
             'client_secret': SPOTIFY_CLIENT_SECRET
         }
-
         response = requests.post(TOKEN_URL, data=req_body)
         token_info = response.json()
 
@@ -59,38 +54,37 @@ def callback():
         session['refresh_token'] = token_info['refresh_token']
         session['expires_at'] = datetime.datetime.now().timestamp() + token_info['expires_in']
 
-        return redirect('/search')
+        return redirect(url_for('weather_routes.weather_form'))
     else:
         return jsonify({"error": "No code provided"})
-#helper function, check if we have a token, need to refresh or need to login    
+
+# Helper function to check token
 def get_access_token():
     if 'access_token' not in session:
-        return redirect('login')
-    if datetime.datetime.now().timestamp() > session ['expires_at']:
-        return redirect ('/refresh-token')
+        return redirect(url_for('spotify_routes.login'))
+    if datetime.datetime.now().timestamp() > session['expires_at']:
+        return redirect(url_for('spotify_routes.refresh_token'))
     return session['access_token']
-
+#refresh token if our session expired
 @spotify_routes.route('/refresh-token', methods=['GET'])
 def refresh_token():
     if 'refresh_token' not in session:
-        return redirect('/login')
+        return redirect(url_for('spotify_routes.login'))
 
-    if datetime.datetime.now().timestamp() > session['expires_at']:
-        req_body = {
-            'grant_type': 'refresh_token',
-            'refresh_token': session['refresh_token'],
-            'client_id': SPOTIFY_CLIENT_ID,
-            'client_secret': SPOTIFY_CLIENT_SECRET
-        }
+    req_body = {
+        'grant_type': 'refresh_token',
+        'refresh_token': session['refresh_token'],
+        'client_id': SPOTIFY_CLIENT_ID,
+        'client_secret': SPOTIFY_CLIENT_SECRET
+    }
+    response = requests.post(TOKEN_URL, data=req_body)
+    new_token_info = response.json()
 
-        response = requests.post(TOKEN_URL, data=req_body)
-        new_token_info = response.json()
+    session['access_token'] = new_token_info['access_token']
+    session['expires_at'] = datetime.datetime.now().timestamp() + new_token_info['expires_in']
 
-        session['access_token'] = new_token_info['access_token']
-        session['expires_at'] = datetime.datetime.now().timestamp() + new_token_info['expires_in']
-
-        return redirect('/search')
-
+    return redirect(url_for('weather_routes.weather_form'))
+#search for the top 50 playlist and return the song qualities of that list. 
 @spotify_routes.route('/search', methods=['GET'])
 def get_top_50_playlist():
     country = request.args.get('country')
@@ -104,7 +98,7 @@ def get_top_50_playlist():
         response = requests.get(API_BASE_URL + 'search', headers=headers, params=params)
 
         if response.status_code != 200:
-            return jsonify({'error': 'Failed to fetch top 50 playlist form spotify'}), response.status_code
+            return jsonify({'error': 'Failed to fetch top 50 playlist from Spotify'}), response.status_code
         
         data = response.json()
         if not data['playlists']['items']: 
@@ -119,7 +113,7 @@ def get_top_50_playlist():
 
 def get_playlist_tracks(playlist_id):
     access_token = get_access_token()
-    if isinstance(access_token, str):  # Check if we received a valid access token
+    if isinstance(access_token, str):  
         headers = {'Authorization': f'Bearer {access_token}'}
         response = requests.get(f"{API_BASE_URL}playlists/{playlist_id}/tracks", headers=headers)
 
@@ -146,25 +140,3 @@ def get_audio_features(track_ids):
         return jsonify(audio_features)
     else:
         return access_token  # Return the redirect response
-
-
-@spotify_routes.route('/playlists', methods=['GET'])
-def get_playlists():
-    if 'access_token' not in session:
-        return redirect('/login')
-
-    if datetime.datetime.now().timestamp() > session['expires_at']:
-        return redirect('/refresh-token')
-
-    access_token = session['access_token']
-
-    headers = {
-        'Authorization': f'Bearer {access_token}'
-    }
-
-    response = requests.get(API_BASE_URL + 'me/playlists', headers=headers)
-    playlists = response.json()
-
-    return jsonify(playlists)
-
-
