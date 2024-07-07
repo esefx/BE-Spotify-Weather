62afe49aac14417c945fee1ee3e7552398bd7817
Your Name <susi.franco@cerebral.com>
2024-07-07 13:38:03 -0500

    fix routes and user model

--- routes/spotify.py
+++ routes/spotify.py
@@ -1,9 +1,11 @@
+
 from flask import Blueprint, redirect, request, jsonify, url_for
 import os
 import logging
 import urllib.parse
 import datetime
 import requests
+from routes.spotify_auth import get_user_from_token, get_access_token
 
 
 logging.basicConfig(level=logging.INFO)
@@ -13,113 +15,28 @@ AUTH_URL = 'https://accounts.spotify.com/authorize'
 TOKEN_URL = 'https://accounts.spotify.com/api/token'
 API_BASE_URL = 'https://api.spotify.com/v1/'
 
-# Get Spotify API keys
-SPOTIFY_CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
-SPOTIFY_CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
-SPOTIFY_REDIRECT_URI = os.getenv('SPOTIFY_REDIRECT_URI')
-SPOTIFY_SCOPES = os.getenv('SPOTIFY_SCOPES')
-ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
-REFRESH_TOKEN = os.getenv('REFRESH_TOKEN')
-EXPIRES_AT = os.getenv('EXPIRES_AT')
-USER_ID = os.getenv('USER_ID')
 
 spotify_routes = Blueprint('spotify_routes', __name__)
 
-# Handle login
-@spotify_routes.route('/login', methods=['GET'])
-def login():
-    params = {
-        'client_id': SPOTIFY_CLIENT_ID,
-        'response_type': 'code',
-        'redirect_uri': SPOTIFY_REDIRECT_URI,
-        'scope': SPOTIFY_SCOPES,
-        'show_dialog': True
-    }
-    auth_url = f"{AUTH_URL}?{urllib.parse.urlencode(params)}"
-    return jsonify({'auth_url': auth_url})
-
-# Handle if login is successful or not
-@spotify_routes.route('/callback', methods=['POST'])
-def callback():
-    data = request.json
-    if 'error' in data:
-        return jsonify({"error": data['error']})
-    
-    if 'code' in data:
-        req_body = {
-            'code': data['code'],
-            'grant_type': 'authorization_code',
-            'redirect_uri': SPOTIFY_REDIRECT_URI,
-            'client_id': SPOTIFY_CLIENT_ID,
-            'client_secret': SPOTIFY_CLIENT_SECRET
-        }
-        response = requests.post(TOKEN_URL, data=req_body)
-        token_info = response.json()
-
-        if 'access_token' in token_info:
-            os.environ['ACCESS_TOKEN'] = str(token_info['access_token'])
-            os.environ['REFRESH_TOKEN'] = str(token_info['refresh_token'])
-            os.environ['EXPIRES_AT'] = str(datetime.datetime.now().timestamp() + token_info['expires_in'])
-
-
-             # Get the user ID
-            headers = {'Authorization': f'Bearer {token_info["access_token"]}'}
-            response = requests.get(API_BASE_URL + 'me', headers=headers)
-            os.environ['USER_ID'] = response.json()['id']
-
-            return jsonify({"login_status": "successful"})
-        else:
-            return jsonify({"error": token_info.get('error', 'Failed to retrieve access token')})
-    else:
-        return jsonify({"error": "No code provided"})
-    
-# Helper function to check token
-def get_access_token():
-    # First, try to get the access token from the headers
-    auth_header = request.headers.get('Authorization')
-    if auth_header:
-        return auth_header.split(' ')[1]
-
-    # If the access token is not in the headers, fall back to the environment variables
-    if ACCESS_TOKEN != str:
-        return redirect(url_for('spotify_routes.login'))
-    if datetime.datetime.now().timestamp() > float(os.environ['EXPIRES_AT']):
-        return redirect(url_for('spotify_routes.refresh_token'))
-    
-    return ACCESS_TOKEN
-
-#refresh token if our session expired
-@spotify_routes.route('/refresh-token', methods=['GET'])
-def refresh_token():
-    if 'REFRESH_TOKEN' not in os.environ:
-        return redirect(url_for('spotify_routes.login'))
-
-    req_body = {
-        'grant_type': 'refresh_token',
-        'refresh_token': os.environ['REFRESH_TOKEN'],
-        'client_id': SPOTIFY_CLIENT_ID,
-        'client_secret': SPOTIFY_CLIENT_SECRET
-    }
-    response = requests.post(TOKEN_URL, data=req_body)
-    new_token_info = response.json()
-
-    os.environ['ACCESS_TOKEN'] = str(new_token_info['access_token'])
-    os.environ['EXPIRES_AT'] = str(datetime.datetime.now().timestamp() + new_token_info['expires_in'])
-
-    return redirect(url_for('weather_routes'))
 
 #search for the top 50 playlist and return the song qualities of that list. 
 @spotify_routes.route('/search', methods=['GET'])
 def get_top_50_playlist():
+    access_token = request.headers.get('Authorization').split(' ')[1]
+    user = get_user_from_token(access_token)
+    user_id = user['id']
 
+
+    if not user_id or not access_token:
+        return jsonify({'error': 'Missing user_id or access_token'}), 400
     country = request.args.get('country')
     if not country:
         return jsonify({"error": "Country parameter is missing"}), 400
 
-    headers = {'Authorization': f'Bearer {ACCESS_TOKEN}'}
+    headers = {'Authorization': f'Bearer {access_token}'}
     params = {'q': f'top 50 {country}', 'type': 'playlist', 'limit': 1}
     response = requests.get(API_BASE_URL + 'search', headers=headers, params=params)
-    print(f"ACCESS_TOKEN: {ACCESS_TOKEN}")  # Debug print
+
  
     if response.status_code != 200:
         return jsonify({'error': 'Failed to fetch top 50 playlist from Spotify'}), response.status_code
@@ -129,10 +46,10 @@ def get_top_50_playlist():
         return jsonify({'error': 'No playlist found'}), 404
 
     playlist_id = data['playlists']['items'][0]['id']
-    return get_playlist_tracks(playlist_id)
+    return get_playlist_tracks(playlist_id, access_token)
 
-def get_playlist_tracks(playlist_id):
-        headers = {'Authorization': f'Bearer {ACCESS_TOKEN}'}
+def get_playlist_tracks(playlist_id, access_token):
+        headers = {'Authorization': f'Bearer {access_token}'}
         response = requests.get(f"{API_BASE_URL}playlists/{playlist_id}/tracks", headers=headers)
         logging.info(f'Spotify response: {response.status_code}, {response.text}')
 
@@ -143,20 +60,18 @@ def get_playlist_tracks(playlist_id):
         track_ids = [item['track']['id'] for item in data['items']]
         logging.info(f'Track IDs: {track_ids}')
 
-        return get_audio_features(track_ids)
+        return get_audio_features(track_ids, access_token)
 
 
-def get_audio_features(track_ids):
-        headers = {'Authorization': f'Bearer {ACCESS_TOKEN}'}
+def get_audio_features(track_ids, access_token):
+        headers = {'Authorization': f'Bearer {access_token}'}
         params = {'ids': ','.join(track_ids)}
         response = requests.get(f"{API_BASE_URL}audio-features", headers=headers, params=params)
-        logging.info(f'Spotify response: {response.status_code}, {response.text}')
 
         if response.status_code != 200:
             return jsonify({"error": "Failed to fetch audio features from Spotify"}), response.status_code
 
         data = response.json()
-        logging.info(f'Audio features: {data}')
         if 'audio_features' not in data:
             return jsonify({"error": "Failed to extract audio features from Spotify response"})
 
@@ -164,30 +79,37 @@ def get_audio_features(track_ids):
     
     # Create a new Spotify playlist
 @spotify_routes.route('/create-playlist', methods=['POST'])
-def create_playlist():
-    data = request.json
-    playlist_name = data.get('playlist_name')
-    access_token = get_access_token()
-
+def create_playlist(access_token, playlist_name):
+    print("inside create_playlist")
+    user = get_user_from_token(access_token)
+    if not user or 'id' not in user:
+        return jsonify({"error": "User not found or invalid access token"}), 404
+    user_id = user['id']
+    print("user_id", user_id)
+    
     response = requests.post(
-        f"{API_BASE_URL}users/{USER_ID}/playlists",
+        f"{API_BASE_URL}users/{user_id}/playlists",
         headers={'Authorization': f'Bearer {access_token}'},
-        json={'name': playlist_name, 'description': 'Generated by WeatherTunes', 'public': False}
+        json={'name': playlist_name, 'description': 'Generated by WeatherBeats', 'public': True}
     )
+    print("Response status code", response.status_code)
+    print("response message", response.json())
     if response.status_code != 201:
         return jsonify({"error": "Failed to create playlist"}), response.status_code
 
-    playlist_id = response.json()['id']
-    return jsonify({'playlist_id': playlist_id, 'playlist_name': playlist_name})
+    # playlist_id = response.json()['id']
+    data = response.json()
+    print(data)
+    return jsonify({'playlist_id': data, 'playlist_name': playlist_name})
 
 # Add tracks to a Spotify playlist
 @spotify_routes.route('/add-tracks', methods=['POST'])
-def add_tracks_to_playlist():
+def add_tracks_to_playlist(access_token):
+    print("inside add_tracks_to_playlist")
     data = request.json
     playlist_id = data.get('playlist_id')
     track_uris = data.get('track_uris')
     playlist_url = data.get('playlist_url')
-    access_token = get_access_token()
 
     response = requests.post(
         f"{API_BASE_URL}playlists/{playlist_id}/tracks",
--- routes/weather.py
+++ routes/weather.py
@@ -1,19 +1,17 @@
-from flask import Blueprint, request, jsonify, render_template, session
+from flask import Blueprint, request, jsonify
 import requests
 import os
 import logging
-from routes.spotify import create_playlist, add_tracks_to_playlist
-
-logging.basicConfig(level=logging.DEBUG)
+from app.routes.spotify import create_playlist, add_tracks_to_playlist
+from app.routes.spotify_auth import get_user_from_token
+from flask_cors import cross_origin
 
 weather_routes = Blueprint('weather_routes', __name__)
 
-ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
-# user_id = os.getenv('USER_ID')
+def filter_songs_by_weather(song_qualities, weather_condition, temperature):
+    print("Inside filter_songs_by_weather")
 
-def filter_songs_by_weather(weather_data, song_qualities):
-    temperature = weather_data['main']['temp']
-    weather_condition = weather_data['weather'][0]['main']
+    temperature = float(temperature)
 
     if temperature > 30:
         return [song for song in song_qualities if song['energy'] > 0.7 and song['valence'] > 0.7]
@@ -26,61 +24,66 @@ def filter_songs_by_weather(weather_data, song_qualities):
     else:
         return [song for song in song_qualities if 0.3 <= song['energy'] <= 0.7 and 0.3 <= song['valence'] <= 0.7]
 
+def get_api_key(api_name):
+    return os.getenv(f'{api_name}_API_KEY')
+
+def call_api(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.json()
+
+def get_location_data(city):
+    api_key = get_api_key('LOCATIONIQ')
+    url = f"https://us1.locationiq.com/v1/search.php?key={api_key}&q={city}&format=json"
+    return call_api(url)
+
+def get_weather_data(lat, lon):
+    api_key = get_api_key('OPENWEATHER')
+    url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&units=metric"
+    return call_api(url)
+
+def get_spotify_data(country, access_token):
+    params = {'country': country}
+    headers = {'Authorization': f'Bearer {access_token}'}
+    url = "http://127.0.0.1:5000/search"
+    response = requests.get(url, params=params, headers=headers)
+    if response.status_code != 200:
+        raise Exception("Failed to fetch Spotify data")
+    return response.json()
+
+def create_and_populate_playlist(playlist_name, songs,  access_token):
+    print("inside create_and_populate_playlist")
+    playlist_info = create_playlist(access_token, playlist_name)
+    playlist_id = playlist_info['playlist_id']
+    track_uris = [song['uri'] for song in songs]
+    add_tracks_to_playlist(playlist_id, track_uris, access_token)
+    return playlist_id
+
 @weather_routes.route('/weather', methods=['POST'])
+@cross_origin(supports_credentials=True, origins='http://localhost:3000')  
 def get_weather():
     try:
-
         city = request.json.get('city')
-        if not city or not isinstance(city, str):
+        if not city:
             return jsonify({'error': 'Invalid or missing city parameter'}), 400
+        access_token = request.headers.get('Authorization').split(' ')[1]
+        user = get_user_from_token(access_token)
+        if not user:
+            return jsonify({'error': 'Access token not found'}), 400
 
-        LOCATIONIQ_API_KEY = os.getenv('LOCATIONIQ_API_KEY')
-        OPENWEATHER_API_KEY = os.getenv('OPENWEATHER_API_KEY')
+        location_data = get_location_data(city)
+        weather_data = get_weather_data(location_data[0]['lat'], location_data[0]['lon'])
+        playlist_name = f"{city} {weather_data['weather'][0]['description']}"
 
-        locationiq_url = f"https://us1.locationiq.com/v1/search.php?key={LOCATIONIQ_API_KEY}&q={city}&format=json"
-        logging.debug(f"Calling LocationIQ API with URL: {locationiq_url}")
-        locationiq_response = requests.get(locationiq_url)
-        locationiq_response.raise_for_status()
-        locationiq_data = locationiq_response.json()
+       
+        spotify_song_data = get_spotify_data(location_data[0]['display_name'].split(',')[-1].strip(), access_token)
+        temperature = weather_data['main']['temp']
+        weather_condition = weather_data['weather'][0]['main']
+        playlist_songs = filter_songs_by_weather(spotify_song_data, weather_condition, temperature)
 
-        lat = locationiq_data[0]['lat']
-        lon = locationiq_data[0]['lon']
-        location_name = locationiq_data[0]['display_name']
-        name_list = location_name.split(',')
-        country = name_list[-1].strip()
+        playlist_id = create_and_populate_playlist(playlist_name, playlist_songs, access_token)
 
-        openweather_url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={OPENWEATHER_API_KEY}&units=metric"
-        logging.debug(f"Calling OpenWeather API with URL: {openweather_url}")
-        openweather_response = requests.get(openweather_url)
-        openweather_response.raise_for_status()
-        openweather_data = openweather_response.json()
-        # Define playlist name
-        weather_description = openweather_data['weather'][0]['description']
-        playlist_name = f"{city} {weather_description}"
-
-        temperature = openweather_data['main']['temp']
-
-        search_response = requests.get(f"http://127.0.0.1:5000/search?country={country}")
-        if search_response.status_code != 200:
-            return jsonify({"error": "Failed to fetch Spotify data"}), search_response.status_code
-
-        song_qualities = search_response.json()
-
-        # Filter the songs based on the weather
-        playlist = filter_songs_by_weather(openweather_data, song_qualities)
-
-        # Create a new Spotify playlist
-        playlist_info = create_playlist(playlist_name)
-        playlist_id = playlist_info['playlist_id']
-
-
-        # Add tracks to the new Spotify playlist
-        track_uris = [song['uri'] for song in playlist]
-        add_tracks_info = add_tracks_to_playlist(playlist_id, track_uris)
-        playlist_id = add_tracks_info['playlist_id']
-
-        return jsonify({'temperature': temperature, 'playlist': playlist_id})
-
+        return jsonify({'temperature': weather_data['main']['temp'], 'playlist': playlist_id})
     except Exception as e:
         logging.error(f"Internal server error: {e}")
         return jsonify({'error': 'Internal server error'}), 500
