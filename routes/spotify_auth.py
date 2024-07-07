ff3adceec5616094e3da84f98d5bca86107af6ca
Your Name <susi.franco@cerebral.com>
2024-07-07 18:39:47 -0500

    fix spotify auth to follow PKCE flow

--- routes/spotify_auth.py
+++ routes/spotify_auth.py
@@ -1,39 +1,63 @@
 from flask import Blueprint, redirect, request, jsonify, url_for, make_response
 from sqlalchemy.orm.exc import NoResultFound
-from datetime import datetime
+from datetime import datetime, timedelta
 import urllib.parse
+from urllib.parse import urlencode
 import os
 import requests
 from models.user import User
+from models.temp import TemporaryStorage  
 from app import db
 from flask_cors import cross_origin
+import base64
+import hashlib
 
-# Spotify API endpoints
+# Spotify API endpoints and credentials
 AUTH_URL = 'https://accounts.spotify.com/authorize'
-TOKEN_URL = 'https://accounts.spotify.com/api/token?'
+TOKEN_URL = 'https://accounts.spotify.com/api/token'
 API_BASE_URL = 'https://api.spotify.com/v1/'
-
-# Get Spotify API keys
 SPOTIFY_CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
-SPOTIFY_CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
 SPOTIFY_REDIRECT_URI = os.getenv('SPOTIFY_REDIRECT_URI')
 SPOTIFY_SCOPES = os.getenv('SPOTIFY_SCOPES')
 
-
 spotify_auth_routes = Blueprint('spotify_auth_routes', __name__)
 
-# Handle login
+def generate_code_verifier_and_challenge():
+    code_verifier = base64.urlsafe_b64encode(os.urandom(32)).rstrip(b'=').decode('utf-8')
+    code_challenge = base64.urlsafe_b64encode(hashlib.sha256(code_verifier.encode('utf-8')).digest()).rstrip(b'=').decode('utf-8')
+    return code_verifier, code_challenge
+
+def generate_unique_session_id():
+    return base64.urlsafe_b64encode(os.urandom(32)).rstrip(b'=').decode('utf-8')
+
 @spotify_auth_routes.route('/login', methods=['GET'])
+@cross_origin(supports_credentials=True, origins='http://localhost:3000')
 def login():
+    code_verifier, code_challenge = generate_code_verifier_and_challenge()
     params = {
         'client_id': SPOTIFY_CLIENT_ID,
         'response_type': 'code',
         'redirect_uri': SPOTIFY_REDIRECT_URI,
         'scope': SPOTIFY_SCOPES,
-        'show_dialog': True
+        'code_challenge_method': 'S256',
+        'code_challenge': code_challenge,
+        'show_dialog': 'true'
     }
     auth_url = f'{AUTH_URL}?{urllib.parse.urlencode(params)}'
-    return jsonify({'auth_url': auth_url})
+    session_id = generate_unique_session_id()
+    
+    # Calculate the expiration time (e.g., 1 hour from now)
+    expires_at = datetime.utcnow() + timedelta(hours=1)
+    
+    temp_storage = TemporaryStorage(key=session_id, value=code_verifier, expires_at=expires_at)
+    db.session.add(temp_storage)
+    db.session.commit()
+    
+    response = make_response(jsonify({'auth_url': auth_url, 'session_id': session_id}))
+    response.set_cookie('session_id', value=session_id, secure=False, httponly=False, samesite='Lax')
+    
+    
+    return response
 
 @spotify_auth_routes.route('/close')
 def close_popup():
@@ -45,19 +69,24 @@ def close_popup():
               </body></html>'''
 
 @spotify_auth_routes.route('/callback', methods=['GET'])
-@cross_origin(supports_credentials=True, origins='http://localhost:3000')  # Adjust the origins as per your frontend's URL
-
+@cross_origin(supports_credentials=True, origins='http://localhost:3000')
 def callback():
-    print("start of callback")
     code = request.args.get('code')
-    print("Authorization code:", code)
-
     if not code:
         return jsonify({"error": "Authorization code not provided"}), 400
 
-    token_info = exchange_code_for_access_token(code)
+    session_id = request.cookies.get('session_id')
+    print(session_id)
+      # Assuming session_id is stored in cookies
+    temp_storage = TemporaryStorage.query.filter_by(key=session_id).first()
+    if not temp_storage:
+        return jsonify({"error": "Session not found"}), 400
+    code_verifier = temp_storage.value
+
+    token_info = exchange_code_for_access_token(code, code_verifier)
     if 'access_token' not in token_info:
         return jsonify({"error": token_info.get('error', 'Failed to retrieve access token')}), 400
+
     access_token = token_info['access_token']
     user_id = fetch_user_id(access_token)
     if not user_id:
@@ -66,22 +95,21 @@ def callback():
     user = update_or_create_user(user_id, token_info)
     if not user:
         return jsonify({"error": "Failed to update or create user"}), 500
+
     return prepare_response(access_token)
 
-def prepare_response(access_token):
-    response = make_response(redirect(url_for('spotify_auth_routes.close_popup')))
-    response.set_cookie('accessToken', value=access_token, secure=False, httponly=False, samesite='Lax')  
-    return response
-
-def exchange_code_for_access_token(code):
+def exchange_code_for_access_token(code, code_verifier):
     req_body = {
         'grant_type': 'authorization_code',
         'code': code,
         'redirect_uri': SPOTIFY_REDIRECT_URI,
         'client_id': SPOTIFY_CLIENT_ID,
-        'client_secret': SPOTIFY_CLIENT_SECRET,
+        'code_verifier': code_verifier
     }
-    response = requests.post(TOKEN_URL, data=req_body)
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    response = requests.post(TOKEN_URL, data=urlencode(req_body), headers=headers)
     return response.json()
 
 def fetch_user_id(access_token):
@@ -90,32 +118,28 @@ def fetch_user_id(access_token):
     return response.json().get('id')
 
 def update_or_create_user(user_id, token_info):
-    expires_at = datetime.now().timestamp() + token_info['expires_in']
+    expires_at = datetime.now() + timedelta(seconds=token_info['expires_in'])
     user = User.query.filter_by(user_id=user_id).first()
     if user:
         user.access_token = token_info['access_token']
-        user.refresh_token = token_info['refresh_token']
-        user.expires_at = datetime.fromtimestamp(expires_at)
+        user.refresh_token = token_info.get('refresh_token')  # Some flows might not return a new refresh token
+        user.expires_at = expires_at
     else:
         user = User(
             user_id=user_id,
             access_token=token_info['access_token'],
             refresh_token=token_info['refresh_token'],
-            expires_at=datetime.fromtimestamp(expires_at)
+            expires_at=expires_at
         )
         db.session.add(user)
     db.session.commit()
     return user
 
+def prepare_response(access_token):
+    response = make_response(redirect(url_for('spotify_auth_routes.close_popup')))
+    response.set_cookie('accessToken', value=access_token, secure=False, httponly=False, samesite='Lax')  
+    return response
 
-#get access token from request cookies
-def get_access_token():
-    access_token = request.cookies.get('accessToken')
-    if access_token:
-        return access_token
-    else:
-        return {'error': 'Access token not found'}, 400
-    
 # Helper function to get the user from the access token
 def get_user_from_token(access_token):
     if access_token:
@@ -127,54 +151,4 @@ def get_user_from_token(access_token):
         else:
             # Redirect to token refresh if the token has expired or no user is found
             return redirect(url_for('spotify_auth_routes.refresh_token'))
-
-def update_access_token_in_db(user_id, new_access_token, new_expires_at):
-    try:
-        # Assuming user_identifier is an access token, adjust if it's a user ID or another identifier
-        user = User.query.filter_by(user_id=user_id).one()
-        user.access_token = new_access_token
-        user.expires_at = new_expires_at
-        db.session.commit()
-    except NoResultFound:
-        # Handle the case where no user is found, e.g., log an error or raise an exception
-        pass
-
-#refresh token if our session expired
-@spotify_auth_routes.route('/refresh_token', methods=['GET'])
-def refresh_token():
-    access_token = get_access_token()
-
-    if not access_token:
-        # No access token provided, redirect to login
-        return redirect(url_for('spotify_auth_routes.login'))
-
-    #Identify the user by the access token
-    user = get_user_from_token(access_token)
-    if not user:
-        # Access token does not match any user, redirect to login
-        return redirect(url_for('spotify_auth_routes.login'))
-
-    # Retrieve the user's refresh token from the database
-    refresh_token = user.refresh_token
-    if not refresh_token:
-        # No refresh token found for the user, redirect to login
-        return redirect(url_for('spotify_auth_routes.login'))
-
-    # Request a new access token using the refresh token
-    req_body = {
-        'grant_type': 'refresh_token',
-        'refresh_token': refresh_token,
-        'client_id': SPOTIFY_CLIENT_ID,
-        'client_secret': SPOTIFY_CLIENT_SECRET
-    }
-    response = requests.post(TOKEN_URL, data=req_body)
-    new_token_info = response.json()
-
-    # Update the database with the new access token and its expiry
-    update_access_token_in_db(user.user_id, new_token_info['access_token'], datetime.datetime.now().timestamp() + new_token_info['expires_in'])
-
-    # return the new access token to the frontend
-    response = make_response(redirect(url_for('your_redirect_route')))
-    response.set_cookie('access_token', new_token_info['access_token'], max_age=new_token_info['expires_in'])
-
-    return response
+        
